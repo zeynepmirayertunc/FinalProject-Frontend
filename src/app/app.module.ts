dc5939fa14cde4c979e9dc3b86490c390aa6ca38
zeynepmirayertunc <69868488+zeynepmirayertunc@users.noreply.github.com>
2021-03-25 00:05:50 +0300

    25 Mart/March 25

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations"
 
@@ -14,8 +14,9 @@ import { FilterPipePipe } from './pipes/filter-pipe.pipe';
 import { ProductAddComponent } from './components/product-add/product-add.component';
 
 import {ToastrModule} from "ngx-toastr";
-import { from } from 'rxjs';
 import { CartSummaryComponent } from './components/cart-summary/cart-summary.component';
+import { LoginComponent } from './components/login/login.component';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 @NgModule({
   declarations: [
@@ -26,8 +27,8 @@ import { CartSummaryComponent } from './components/cart-summary/cart-summary.com
     VatAddedPipe,
     FilterPipePipe,
     CartSummaryComponent,
-    
     ProductAddComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
@@ -40,7 +41,9 @@ import { CartSummaryComponent } from './components/cart-summary/cart-summary.com
     }),
     BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [
+    {provide:HTTP_INTERCEPTORS, useClass:AuthInterceptor, multi:true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
